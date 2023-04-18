b976161ab961cbab937af39b702c9010fda9dfdf
ivanov6444 <a.ivanov6444@gmail.com>
2023-04-18 13:22:59 +0300

    change search method

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,7 +6,7 @@ import {
   OnInit,
   ViewChild
 } from '@angular/core';
-import {FormBuilder, FormControl} from '@angular/forms'
+import {FormBuilder} from '@angular/forms';
 import {HttpClient} from "@angular/common/http";
 import {
   fromEvent,
@@ -14,9 +14,11 @@ import {
   takeUntil,
   tap,
   debounceTime,
-  delay
+  delay, map
 } from "rxjs";
+import { distinctUntilChanged } from 'rxjs/operators';
 import {SearchService} from "../search.service";
+import {NextOrPrevSibling} from '../next-or-prev-sibling';
 
 @Component({
   selector: 'app-search',
@@ -33,7 +35,7 @@ export class SearchComponent implements OnInit, OnDestroy{
    isLoading = false;
    isNotResult = false;
    isSelectedItem = false;
-   searchInputControl: FormControl = this.fb.control('', []);
+   searchInputControl = this.fb.nonNullable.control('', []);
    resultSearchList : any[] = [];
    infiniteScrollObserver = new IntersectionObserver(
     ([entry], observer) =>{
@@ -41,12 +43,13 @@ export class SearchComponent implements OnInit, OnDestroy{
         observer.unobserve(entry.target);
         if(this.remainItem > 0){
           this.isLoading = true;
-          this.loadMorePage(this.searchInputControl.value, this.nextPage++);
+          this.goToSearch(this.searchInputControl.value, this.nextPage++);
         }
         this.ref.markForCheck();
       }
     }, {threshold: 1}
-  )
+  );
+
   private destroy$ = new Subject();
   private nextPage = 2;
   private remainItem = 0;
@@ -57,76 +60,81 @@ export class SearchComponent implements OnInit, OnDestroy{
     private httpClient: HttpClient,
     private searchService: SearchService,
     private elementRef: ElementRef,
-    private ref: ChangeDetectorRef) {}
+    private ref: ChangeDetectorRef) {};
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.init();
-  }
+  };
 
-  init(){
-    const searchResult = fromEvent<KeyboardEvent>(this.inputSearch.nativeElement,'keyup' ).pipe(
-      tap( (event:KeyboardEvent) => {
-        if(this.searchInputControl.value.length > 0 && event.code !== 'ArrowDown' && event.code !== 'ArrowLeft' && event.code !== 'ArrowRight'){
-          this.isLoading = true;
-          this.ref.markForCheck();
+  init(): void{
+    fromEvent<InputEvent>(this.inputSearch.nativeElement,'input').pipe(
+      tap((event : InputEvent) => {
+        this.isLoading = true;
+        if(this.inputSearch.nativeElement.value.length === 0){
+              this.isLoading = false;
+              this.resultSearchList = [];
+              this.isSelectedItem = false;
+              this.ref.markForCheck();
         }
-        if(event.code === 'ArrowDown'){
-          this.isLoading = false;
-          this.focusResultFirstItem();
-          this.ref.markForCheck();
-        }
+        this.ref.markForCheck();
       }),
-      debounceTime(1500),
-      tap((event:KeyboardEvent) => {
-        if(this.searchInputControl.value.length === 0){
-          this.isLoading = false;
-          this.resultSearchList = [];
-          this.ref.markForCheck();
-        }
-
-        if(this.searchInputControl.value.length > 0 && event.code !== 'ArrowDown'){
-          this.isNotResult = false;
-          this.isSelectedItem = false;
-          this.goToSearch(this.searchInputControl.value);
-          this.ref.markForCheck();
+      map(() => this.searchInputControl.value),
+      distinctUntilChanged(),
+      debounceTime(1000),
+      tap((searchValue : string) => {
+        if(searchValue.length > 0){
+          this.goToSearch(searchValue);
         }
       }),
       takeUntil(this.destroy$)
-    );
-
-    searchResult.subscribe();
+    ).subscribe()
   }
 
-  getErrorMessage() {
+  getErrorMessage(): string {
     return this.searchInputControl.hasError('pattern') ? 'only letters of the Latin alphabet' : '';
   }
 
-  goToSearch(searchText: string):void{
-    this.nextPage = 2;
-
-    if(this.searchBlock?.nativeElement.firstElementChild && this.searchBlock.nativeElement.scrollTop > 0){
-      this.searchBlock.nativeElement.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-      });
+  goToSearch(searchText: string, page?: number):void{
+    if(!page){
+      this.nextPage = 2;
+      if(this.searchBlock?.nativeElement.firstElementChild && this.searchBlock.nativeElement.scrollTop > 0){
+        this.searchBlock.nativeElement.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: "smooth",
+        });
+      }
     }
 
-    this.searchService.getResult(searchText).pipe(
-      tap((res) => {
-       if(res.items){
+    this.searchService.getResult(searchText,page).pipe(
+      tap((searchResult) => {
+       if(searchResult.items){
          this.isLoading = false;
-         this.remainItem = res.totalItems - res.endIndex;
-         this.resultSearchList = res.items
+         this.isSelectedItem = true;
+         this.remainItem = searchResult.totalItems - searchResult.endIndex;
+
+         if(this.resultSearchList.length === 0){
+           this.resultSearchList = searchResult.items;
+         }else{
+           this.resultSearchList.push(...searchResult.items);
+         }
+
          this.ref.markForCheck();
        }
-       if(res.items.length === 0){
+
+       if(searchResult.items.length === 0){
          this.isNotResult = true;
+         this.resultSearchList = [];
+         this.ref.markForCheck();
+       }else{
+         this.isNotResult = false;
+         this.ref.markForCheck();
        }
+
       }),
       delay(1000),
       tap(() => {
-        if(this.searchResult?.nativeElement.lastElementChild !== null){
+        if(this.searchResult && this.searchResult?.nativeElement.lastElementChild !== null){
           this.infiniteScrollObserver.observe(this.searchResult?.nativeElement.lastElementChild);
         }
       }),
@@ -134,85 +142,56 @@ export class SearchComponent implements OnInit, OnDestroy{
     ).subscribe()
   }
 
-  loadMorePage(text:string, page:number){
-    this.searchService.getResult(text, page).pipe(
-      tap(res => {
-        this.isLoading = false;
-        this.remainItem = res.totalItems - res.endIndex;
-        this.resultSearchList.push(...res.items);
-        this.ref.markForCheck();
-      }),
-      delay(1000),
-      tap(() => {
-        if(this.searchResult?.nativeElement.lastElementChild !== null){
-          this.infiniteScrollObserver.observe(this.searchResult?.nativeElement.lastElementChild);
-        }
-      }),
-      takeUntil(this.destroy$)
-    ).subscribe()
-  }
-
-  selectByClick(event: MouseEvent){
+  selectByClick(event: MouseEvent): void{
     const target = event.target as HTMLDivElement;
     this.searchInputControl.setValue(target.innerText);
-    this.isSelectedItem = true;
+    this.isSelectedItem = false;
     this.ref.markForCheck()
   }
 
-  selectByKey(event: KeyboardEvent){
+  selectByKey(event: KeyboardEvent): void{
+
     if(event.code === 'Enter' || event.code === 'NumpadEnter'){
       const target = event.target as HTMLDivElement;
       this.searchInputControl.setValue(target.innerText);
-      this.isSelectedItem = true;
+      this.isSelectedItem = false;
       this.ref.markForCheck()
     }
-    if(event.code === 'ArrowUp'){
+
+    if(event.code === 'ArrowUp' || event.code === 'ArrowDown'){
       event.preventDefault();
-      this.focusPreviousItem();
-    }
-    if(event.code === 'ArrowDown'){
-      event.preventDefault();
-      this.focusNextItem()
+      this.focusSiblingItem(event.code)
     }
   }
 
-  private focusResultFirstItem(){
-    if (this.searchResult.nativeElement.firstElementChild) {
+  focusResultFirstItem(keyup: KeyboardEvent): void{
+    if(keyup.code === 'ArrowDown' && this.searchResult.nativeElement.firstElementChild){
       this.searchResult.nativeElement.firstElementChild.tabIndex = -1;
       this.searchResult.nativeElement.firstElementChild.focus();
-
     }
-  }
+  };
 
+  private focusSiblingItem(keyCode: string): void {
+    const item = document.activeElement as HTMLElement;
 
-  private focusNextItem() {
-    const item  = document.activeElement as HTMLElement;
-    if(item.nextElementSibling){
-      this.activateFocus(item.nextElementSibling as HTMLElement);
-      item.tabIndex = -1;
-    }else{
-      this.activateFocus( this.searchResult?.nativeElement.firstElementChild);
-      item.tabIndex = -1;
+    if(keyCode === 'ArrowUp' || keyCode === 'ArrowDown'){
+
+      if(item[NextOrPrevSibling[keyCode]]){
+        this.activateFocus(item[NextOrPrevSibling[keyCode]] as HTMLElement);
+        item.tabIndex = -1;
+      }else{
+        keyCode === 'ArrowUp'? this.activateFocus( this.searchResult?.nativeElement.lastElementChild) : this.activateFocus( this.searchResult?.nativeElement.firstElementChild);
+        item.tabIndex = -1;
+      }
     }
-  }
+  };
 
-  private focusPreviousItem() {
-    const item  = document.activeElement as HTMLElement;
-    if (item.previousElementSibling) {
-      this.activateFocus(item.previousElementSibling as HTMLElement);
-      item.tabIndex = -1;
-    }else{
-      this.activateFocus( this.searchResult?.nativeElement.lastElementChild);
-      item.tabIndex = -1;
-    }
-  }
-
-  private activateFocus(item:HTMLElement) {
+  private activateFocus(item:HTMLElement): void {
     item.tabIndex = 0;
     item.focus();
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.complete();
   }
